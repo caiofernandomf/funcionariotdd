7b6085e2982760c6d0dca63cd5a18f012142347c
caiofernandomf <caiofernandomf@gmail.com>
2023-08-21 12:10:30 -0300

    test: Added first test

--- src/test/java/io/linuxtips/funcionariotdd/testes/FuncionarioTest.java
+++ src/test/java/io/linuxtips/funcionariotdd/testes/FuncionarioTest.java
@@ -5,11 +5,15 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 @Import(LocalStackConfig.class)
@@ -32,5 +36,20 @@ public class FuncionarioTest {
         return new ResponseSpecBuilder()
                 .expectStatusCode(responseStatus)
                 .build();
+    }
+
+    @Test
+    @DisplayName("Deve cadastrar um funcionario com sucesso")
+    public void deveCriarFuncionarioComSucesso(){
+        try{
+            criaFuncionario(new Funcionario("123","Fernando",4000.0D))
+                    .then()
+                    .assertThat().spec(responseSpecification(201))
+                    .and()
+                    .assertThat()
+                    .body("nome",equalTo("Fernando"));
+        }catch (Exception e){
+            fail("Não foi possível cadastrar um funcionario",e);
+        }
     }
 }
